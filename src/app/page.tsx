b37e3d6c4d4fbf05b1fd4f2cd0bf4e7b5dc812b8
unimu-cic <125171539+unimu-cic@users.noreply.github.com>
2024-02-04 19:02:55 +0800

    feat: improve responsible style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex h-screen bg-black items-center justify-center flex-col text-secondary">
+    <main className="flex h-screen bg-black items-center justify-center flex-col text-secondary px-5">
       <h1 className="text-4xl text-primary font-semibold">
         <span>XERO</span> &nbsp;TODO
       </h1>
-      <h2 className="text-2xl mt-10">Get your things done more simply.</h2>
+      <h2 className="text-2xl mt-10 text-center">Get your things done more simply.</h2>
 
       <Link
         href="/sign-in"
--- src/app/sign-in/[[...sign-in]]/AuthUI.tsx
+++ src/app/sign-in/[[...sign-in]]/AuthUI.tsx
@@ -1,13 +1,13 @@
 "use client";
-import { Auth } from "@supabase/auth-ui-react";
-import { getURL } from "@/utils/helpers";
-import { useSupabase } from "@/app/supabase-provider";
+import {Auth} from "@supabase/auth-ui-react";
+import {getURL} from "@/utils/helpers";
+import {useSupabase} from "@/app/supabase-provider";
 
 const AuthUI = () => {
-  const { supabase } = useSupabase();
+  const {supabase} = useSupabase();
 
   return (
-    <div className="bg-secondary-500 p-6 rounded-lg">
+    <div className="bg-secondary-500 p-4 sm:p-6 rounded-lg">
       <div className="text-black mb-4">
         <h2 className="text-2xl font-semibold">Sign in</h2>
         <p className="opacity-70">to continue to XERO TODO</p>
@@ -21,13 +21,13 @@ const AuthUI = () => {
           className: {
             anchor: "text-xs underline mx-auto text-black-300",
             button:
-              "bg-black justify-center flex text-secondary rounded-lg p-2 px-3 gap-2 hover:bg-black-300 transition-all [&>svg]:h-6 [&>svg]:w-6",
-            container: "w-[20rem] flex flex-col gap-4 mb-4 last:mb-0",
+              "bg-black text-[15px] justify-center items-center flex text-secondary rounded-lg p-2 sm:px-3 gap-2 hover:bg-black-300 transition-all [&>svg]:h-6 [&>svg]:w-6",
+            container: "w-[16rem] sm:w-[20rem] flex flex-col gap-4 mb-2 sm:mb-4 last:mb-0",
             divider:
-              "bg-secondary-300 h-[1px] my-10 relative before:bg-secondary-500 before:content-['or'] before:absolute before:px-2 before:text-black-300 before:top-1/2 before:left-1/2 before:-translate-y-1/2 before:-translate-x-1/2",
+              "bg-secondary-300 h-[1px] my-6 sm:my-10 relative before:bg-secondary-500 before:content-['or'] before:absolute before:px-2 before:text-black-300 before:top-1/2 before:left-1/2 before:-translate-y-1/2 before:-translate-x-1/2",
             input:
-              "text-black flex h-9 w-full rounded-md border border-black transition-all bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-black/70 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring",
-            label: "mb-1 flex flex-col text-sm text-black",
+              "text-black text-[15px] flex h-9 w-full rounded-md border border-black transition-all bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-black/70 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring",
+            label: "text-[13px] mb-1 flex flex-col text-sm text-black",
             message: "text-red-500 text-center text-sm block",
           },
         }}
